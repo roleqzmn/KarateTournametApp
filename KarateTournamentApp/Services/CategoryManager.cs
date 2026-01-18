56f93be43cf3076ce3d223f3445f34b3ee2f9c2c
roleq <maciej.majewski06@gmail.com>
2026-01-18 16:38:58 +0100

    fixed bugs in senior categories creation

--- KarateTournamentApp/Services/CategoryManager.cs
+++ KarateTournamentApp/Services/CategoryManager.cs
@@ -10,58 +10,58 @@ namespace KarateTournamentApp.Services
     public class CategoryManager
     {
         public List<Category> DefinedCategories { get; set; } = new List<Category>();
-
+        private List<Belts> AllBelts { get; } = Enum.GetValues<Belts>().ToList();
         public void AssignParticipant(Participant p, bool DivideByBelt, bool DivideByAge) 
         {
+            if (p.Age >= 18) AssignSenior(p);
             if (DivideByAge) AssignByAge(p);
             if (DivideByBelt) AssignByBelt(p);
             if (DivideByBelt && DivideByAge) AssignByBoth(p);
         }
-        public void AssignByBelt(Participant p)
+        public void AssignSenior(Participant p)
         {
-            foreach (var categoryType in p.Categories)
+            foreach(var categoryType in p.Categories)
             {
-                if (p.Age < 18)
-                {
-                    var targetCategory = DefinedCategories.FirstOrDefault(c =>
-                    c.MaxAge<18 &&
-                    c.AllowedBelts.Contains(p.Belt) &&
-                    (p.Sex == c.Sex || c.Sex == Sex.Unisex) &&
-                    categoryType == c.CategoryType);
+                var targetCategory = DefinedCategories.FirstOrDefault(c =>
+                   c.MaxAge < 18 &&
+                   c.AllowedBelts.Contains(p.Belt) &&
+                   (p.Sex == c.Sex || c.Sex == Sex.Unisex) &&
+                   categoryType == c.CategoryType);
 
-                    if (targetCategory != null) 
-                    {
-                        targetCategory.Participants.Add(p);
-                    }
-                    else
-                    {
-                        StringBuilder sb = new StringBuilder();
-                        sb.Append(p.Belt.ToString());
-                        string name = sb.ToString();
-                        var cat = new Category(name, p.Belt, categoryType, p.Sex, 1, 17);
-                        if (categoryType == CategoryType.Kumite)
-                        {
-                            cat = new ShobuSanbonCategory(name, p.Belt, categoryType, p.Sex, 1, 17);
-                        }
-                        cat.Participants.Add(p);
-                        DefinedCategories.Add(cat);
-                    }
+                if (targetCategory != null)
+                {
+                    targetCategory.Participants.Add(p);
                 }
                 else
                 {
-                    var cat = new Category("Senior", p.Belt, categoryType, p.Sex, 18, 99);
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append(p.Belt.ToString());
+                    string name = sb.ToString();
+                    var cat = new Category(name, p.Belt, categoryType, p.Sex, 1, 17);
                     if (categoryType == CategoryType.Kumite)
                     {
-                        cat = new ShobuSanbonCategory("Senior", p.Belt, categoryType, p.Sex, 18, 99);
+                        cat = new ShobuSanbonCategory(name, p.Belt, categoryType, p.Sex, 1, 17);
                     }
                     cat.Participants.Add(p);
                     DefinedCategories.Add(cat);
                 }
             }
         }
+        public void AssignByBelt(Participant p)
+        {
+            foreach (var categoryType in p.Categories)
+            {
+                var cat = new Category("Senior", AllBelts, categoryType, p.Sex, 18, 99);
+                if (categoryType == CategoryType.Kumite)
+                {
+                    cat = new ShobuSanbonCategory("Senior", AllBelts, categoryType, p.Sex, 18, 99);
+                }
+                cat.Participants.Add(p);
+                DefinedCategories.Add(cat);
+            }
+        }
         public void AssignByAge(Participant p)
         {
-            List<Belts> belts = Enum.GetValues<Belts>().ToList();
             foreach (var categoryType in p.Categories)
             {
                 var targetCategory = DefinedCategories.FirstOrDefault(c =>
@@ -75,31 +75,17 @@ namespace KarateTournamentApp.Services
                     targetCategory.Participants.Add(p);
                 }
                 else
-                {
-                    if (p.Age >= 18)
+                { 
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append(p.Age.ToString());
+                    string name = sb.ToString();
+                    var cat = new Category(name, AllBelts, categoryType, p.Sex, p.Age, p.Age);
+                    if (categoryType == CategoryType.Kumite)
                     {
-                        var cat = new Category("Senior", belts, categoryType, p.Sex, 18, 99);
-                        if (categoryType == CategoryType.Kumite)
-                        {
-                            cat = new ShobuSanbonCategory("Senior", belts, categoryType, p.Sex, 18, 99);
-                        }
-                        cat.Participants.Add(p);
-                        DefinedCategories.Add(cat);
+                        cat = new ShobuSanbonCategory(name, AllBelts, categoryType, p.Sex, p.Age, p.Age);
                     }
-                    else
-                    {
-                        StringBuilder sb = new StringBuilder();
-                        sb.Append(p.Age.ToString());
-                        string name = sb.ToString();
-                        var cat = new Category(name, belts, categoryType, p.Sex, p.Age, p.Age);
-                        if (categoryType == CategoryType.Kumite)
-                        {
-                            cat = new ShobuSanbonCategory(name, belts, categoryType, p.Sex, p.Age, p.Age);
-                        }
-                        cat.Participants.Add(p);
-                        DefinedCategories.Add(cat);
-                    }
-
+                    cat.Participants.Add(p);
+                    DefinedCategories.Add(cat);
                 }
             }
         }
@@ -120,32 +106,18 @@ namespace KarateTournamentApp.Services
                 }
                 else
                 {
-                    if(p.Age>=18)
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append(p.Belt.ToString());
+                    sb.Append(" ");
+                    sb.Append(p.Age.ToString());
+                    string name = sb.ToString();
+                    var cat = new Category(name, p.Belt, categoryType, p.Sex, p.Age, p.Age);
+                    if (categoryType == CategoryType.Kumite)
                     {
-                        List<Belts> belts = Enum.GetValues<Belts>().ToList();
-                        var cat = new Category("Senior", belts, categoryType, p.Sex, 18, 99);
-                        if (categoryType == CategoryType.Kumite)
-                        {
-                            cat = new ShobuSanbonCategory("Senior", belts, categoryType, p.Sex, 18, 99);
-                        }
-                        cat.Participants.Add(p);
-                        DefinedCategories.Add(cat);
+                        cat = new ShobuSanbonCategory(name, p.Belt, categoryType, p.Sex, p.Age, p.Age);
                     }
-                    else
-                    {
-                        StringBuilder sb = new StringBuilder();
-                        sb.Append(p.Belt.ToString());
-                        sb.Append(" ");
-                        sb.Append(p.Age.ToString());
-                        string name = sb.ToString();
-                        var cat = new Category(name, p.Belt, categoryType, p.Sex, p.Age, p.Age);
-                        if (categoryType == CategoryType.Kumite)
-                        {
-                            cat = new ShobuSanbonCategory(name, p.Belt, categoryType, p.Sex, p.Age, p.Age);
-                        }
-                        cat.Participants.Add(p);
-                        DefinedCategories.Add(cat);
-                    }
+                    cat.Participants.Add(p);
+                    DefinedCategories.Add(cat);
                 }
             }
         }
